b84095253e6f9edcb9b9e3ec5197dfe9662d68f1
Tanisha Gupta <tanishag1906@gmail.com>
2026-07-04 16:52:45 +0530

    [Tanisha Gupta] Add:Added UC2 Features

--- AddressBook.java
+++ AddressBook.java
@@ -1,23 +1,3 @@
 public class AddressBook {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Address Book program in Java");
-        Contact person = new Contact();
-        person.firstName = "Tanisha";
-        person.lastName = "Gupta";
-        person.address = "123 Main Street";
-        person.city = "Bulandshahr";
-        person.state = "Uttar Pradesh";
-        person.zip = "203131";
-        person.phoneNumber = "9876543210";
-        person.email = "tanisha.gupta@example.com";
-
-        System.out.println("Contact Details:");
-        System.out.println("Name: " + person.firstName + " " + person.lastName);
-        System.out.println("Address: " + person.address);
-        System.out.println("City: " + person.city);
-        System.out.println("State: " + person.state);
-        System.out.println("Zip: " + person.zip);
-        System.out.println("Phone: " + person.phoneNumber);
-        System.out.println("Email: " + person.email);   
-    }
+    Contact person =new Contact();
 }
