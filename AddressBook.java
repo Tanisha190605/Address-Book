7706c8888998db613dfd2218e077a20cd319fc15
Tanisha Gupta <tanishag1906@gmail.com>
2026-07-04 16:40:25 +0530

    [Tanisha Gupta]Add:Added UC1 Feature

--- AddressBook.java
+++ AddressBook.java
@@ -1,5 +1,14 @@
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println("Welcome to the Address Book program in Java");
+        Contact person = new Contact();
+        person.firstName = "Tanisha";
+        person.lastName = "Gupta";
+        person.address = "123 Main Street";
+        person.city = "Bulandshahr";
+        person.state = "Uttar Pradesh";
+        person.zip = "203131";
+        person.phoneNumber = "9876543210";
+        person.email = "tanisha.gupta@example.com";
     }
 }
---
+++ Contact.java
@@ -0,0 +1,10 @@
+public class Contact {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNumber;
+    String email;
+}
